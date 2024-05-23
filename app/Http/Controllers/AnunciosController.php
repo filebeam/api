f52b331dcfcefff4ed5a868e82d72f978c2ab1f5
Jorge603 <jorgean9999@gmail.com>
2024-05-23 01:41:42 -0600

    Adicion de la API de anuncios

---
+++ app/Http/Controllers/AnunciosController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+
+class AnunciosController extends Controller
+{
+    public function getContent(Request $request)
+    {
+
+        if ($request->isMethod('GET')) { // Método GET
+
+            $announcements = DB::select('SELECT * FROM announcements ORDER BY id DESC');
+
+            return response()->json($announcements);
+        } else {
+            return response('Solo se admiten peticiones GET', 400);
+        }
+    }
+}
---
+++ database/migrations/2024_05_23_013917_create_announcements_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('date');
+            $table->string('title');
+            $table->longText('content');
+            $table->string('imgUrl')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
