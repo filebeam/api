39aad9c7f15015bd6accbf949bcfc23ed73762c6
Jorge603 <jorgean9999@gmail.com>
2024-05-23 02:25:32 -0600

    Cambios sutiles (ya, no diré nada más)

--- app/Http/Controllers/AnunciosController.php
+++ app/Http/Controllers/AnunciosController.php
@@ -9,11 +9,8 @@ class AnunciosController extends Controller
 {
     public function getContent(Request $request)
     {
-
         if ($request->isMethod('GET')) { // Método GET
-
             $announcements = DB::select('SELECT * FROM announcements ORDER BY id DESC');
-
             return response()->json($announcements);
         } else {
             return response('Solo se admiten peticiones GET', 400);
