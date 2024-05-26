9662bc3e9c735abc0bf4761a47832c86619d5f01
Jorge603 <jorgean9999@gmail.com>
2024-05-26 02:03:03 -0600

    Documentacion

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,16 @@ use App\Http\Controllers\AnunciosController;
 use App\Http\Middleware\ValidateFile;
 use App\Http\Middleware\AnnouncementRequests;
 
-Route::get('/', function(){
+Route::get('/', function(){ # Raiz de la pagina
     return view('index');
 });
 
-Route::get('/disclaimer', function(){
+Route::get('/disclaimer', function(){ # Ruta /disclaimer
     return view('disclaimer');
 });
 
-Route::any('/api/anuncios', [AnunciosController::class, 'getContent'])->middleware(AnnouncementRequests::class);
-
+# Ruta /api, controlado por SubmitController y a su vez respaldado por el middleware ValidateFile
 Route::any('/api', [SubmitController::class, 'submit'])->middleware(ValidateFile::class);
+
+# Ruta /api/anuncios, controlado por AnunciosController y a su vez respaldado por el middleware AnnouncementRequests
+Route::any('/api/anuncios', [AnunciosController::class, 'getContent'])->middleware(AnnouncementRequests::class);
