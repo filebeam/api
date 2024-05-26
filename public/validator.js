f64e7afd17eb7d460e7f159c244292302b13a0cc
Jorge603 <jorgean9999@gmail.com>
2024-05-26 00:05:30 -0600

    Por fin limite de 150 MB por subida

--- public/validator.js
+++ public/validator.js
@@ -2,13 +2,13 @@ var fileInput = document.getElementById('fileInput');
 
 fileInput.addEventListener('change', function() {
     var files = fileInput.files;
-    var maxFileSize = 100 * 1024 * 1024; // Tamaño máximo permitido: 100 MB (100 * 1024 * 1024 bytes)
+    var maxFileSize = 150 * 1024 * 1024; // Tamaño máximo permitido: 150 MB (150 * 1024 * 1024 bytes)
 
     for (var i = 0; i < files.length; i++) {
         var fileSize = files[i].size;
 
         if (fileSize > maxFileSize) {
-            alert('El archivo excede el tamaño máximo permitido de 100 MB.');
+            alert('El archivo excede el tamaño máximo permitido de 150 MB.');
             fileInput.value = ''; // Limpiar el campo de entrada de archivo
             return false; // Detener el envío del formulario
         }
