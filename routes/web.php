dc630d676bb4ef6bfae37cb82ca40b3699f8b086
Jorge603 <jorgean9999@gmail.com>
2024-05-30 20:52:23 -0600

    Algo de documentacion

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::any('/api', [SubmitController::class, 'submit'])->middleware(ValidateFile
 # Ruta /api/anuncios, controlado por AnunciosController y a su vez respaldado por el middleware AnnouncementRequests
 Route::any('/api/anuncios', [AnunciosController::class, 'getContent'])->middleware(AnnouncementRequests::class);
 
+# Ruta /github, solo redirige al github de filebeam
 Route::get('/github', function(){
     return redirect()->away('https://github.com/filebeam');
 });
