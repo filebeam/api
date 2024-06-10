312f76c4ea58a1341a6cc95ca09356a6b5f626f5
jorge-603 <jorgean9999@gmail.com>
2024-06-09 22:47:35 -0600

    Remoción de variables de depuracion

--- config/config.php
+++ config/config.php
@@ -5,7 +5,5 @@ require_once "fixurl.php";
 
 $uri = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; # URI generado automaticamente para el funcionamiento de la pagina.
 $uri = sanitize($uri);
-var_dump($uri);
-die;
 $uploadFileDir = '../' . 'file' . '/'; # Directorio donde va a ser alojado el archivo
 $maintenance = true; # Modo mantenimiento
