da4666043036262346449560cbd44edf78307769
jorge-603 <jorgean9999@gmail.com>
2024-07-14 01:28:31 -0600

    Cambios menores

--- announcements.php
+++ announcements.php
@@ -3,15 +3,13 @@
 require "config/config.php";
 require "config/connection.php";
 
-if (!$maintenance) {
-    header('Content-Type: application/json');
-
-    $stmt = $conn->query("SELECT * FROM announcements ORDER BY id DESC");
-
-    $annnouncements = $stmt->fetchAll();
-
-    echo json_encode($annnouncements, JSON_PRETTY_PRINT);
-} else {
+if ($maintenance) {
     echo "Servicio no disponible temporalmente";
     http_response_code(503);
+    return;
 }
+
+header('Content-Type: application/json');
+$stmt = $conn->query("SELECT * FROM announcements ORDER BY id DESC");
+$annnouncements = $stmt->fetchAll();
+echo json_encode($annnouncements, JSON_PRETTY_PRINT);
