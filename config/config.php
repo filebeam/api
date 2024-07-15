8afeeb7b343c8694a7e3778a45f90d02ed9f7796
jorge-603 <jorgean9999@gmail.com>
2024-07-15 00:03:40 -0600

    Cambios menores

--- config/config.php
+++ config/config.php
@@ -5,5 +5,5 @@ require_once "fixurl.php";
 
 $uri = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; # URI generado automaticamente para el funcionamiento de la pagina.
 $uri = sanitize($uri);
-$uploadFileDir = '../' . 'file' . '/'; # Directorio donde va a ser alojado el archivo
+$uploadFileDir = 'file/'; # Directorio donde va a ser alojado el archivo
 $maintenance = false; # Modo mantenimiento
