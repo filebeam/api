2d05739d1b24153b1ae76068d6ef5f175aaba72d
jorge-603 <jorgean9999@gmail.com>
2024-07-15 00:10:14 -0600

    algo de documentacion

--- config/config.php
+++ config/config.php
@@ -4,6 +4,6 @@ require_once "fixurl.php";
 # Variables de configuracion de FileBeam
 
 $uri = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; # URI generado automaticamente para el funcionamiento de la pagina.
-$uri = sanitize($uri);
+$uri = sanitize($uri); # Sanitizado de la URI
 $uploadFileDir = 'file/'; # Directorio donde va a ser alojado el archivo
 $maintenance = false; # Modo mantenimiento
