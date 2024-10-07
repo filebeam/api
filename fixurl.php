0abc38627e4a0636576c6d1184750bb3575eef11
jorge-603 <jorgean9999@gmail.com>
2024-10-06 23:43:04 -0600

    commit

--- fixurl.php
+++ fixurl.php
@@ -3,7 +3,7 @@
 function sanitize($uri){
 
     $uri = htmlspecialchars($uri, ENT_QUOTES, 'UTF-8'); # Sanitizado de la URI
-    $uri = str_replace("/api/", "", $uri); # Ajuste de la URI para evitar problemas con la generacion de URL. 
+    $uri = str_replace("//", "", $uri); # Ajuste de la URI para evitar problemas con la generacion de URL. 
     $uri = strtok($uri, '?'); # Mas ajustes a la URI
 
     return $uri;
