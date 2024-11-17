571e2e5c759b2131d1e712417d5dfbfcbd60d9c5
jorge-603 <jorgean9999@gmail.com>
2024-11-16 23:33:05 -0600

    Fixing cron

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ require_once "fixurl.php";
 
 # Variables de configuracion de FileBeam. Pronto habrán mas funciones y variables de configuracion
 
-$type = "prod"; # prod / dev
+$type = "dev"; # prod / dev
 $uri = $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; # URI generado automaticamente para el funcionamiento de la pagina.
 $uri = sanitize($uri, $type);
 $uploadFileDir = 'file/'; # Directorio donde va a ser alojado el archivo
