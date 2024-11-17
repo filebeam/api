df8e8413714c8e4f8ede9068c288a7ee18f8be73
jorge-603 <jorgean9999@gmail.com>
2024-11-16 22:30:08 -0600

    fixeo

--- config/fixurl.php
+++ config/fixurl.php
@@ -2,8 +2,14 @@
 
 function sanitize($uri){
 
+    include "config.php";
+
     $uri = htmlspecialchars($uri, ENT_QUOTES, 'UTF-8'); # Sanitizado de la URI
-    $uri = str_replace("//", "", $uri); # Ajuste de la URI para evitar problemas con la generacion de URL. 
+    if ($type == "dev") {
+        $uri = str_replace("//", "", $uri); # Ajuste de la URI para evitar problemas con la generacion de URL. 
+    } else {
+        $uri = str_replace("/api/", "", $uri);
+    }
     $uri = strtok($uri, '?'); # Mas ajustes a la URI
 
     return $uri;
