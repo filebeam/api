48a1f1ddc58ac8444d772d5687724d67c74b94ae
jorge-603 <jorgean9999@gmail.com>
2024-11-16 22:45:15 -0600

    control de errores en prod

--- stats.php
+++ stats.php
@@ -2,8 +2,13 @@
 
 # API que permite ver la cantidad de archivos subidos en total
 
+try {
+    
 $directory = 'file/';
 
 $total = count(scandir($directory));
 
 echo $total;
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
