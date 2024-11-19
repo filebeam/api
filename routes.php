721ce2e47edc0b86a15d735782dbab5b5dd4d5b6
jorge-603 <jorgean9999@gmail.com>
2024-11-19 01:33:32 -0600

    CORS de nuevo

--- routes.php
+++ routes.php
@@ -8,6 +8,13 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
 
+# Manejar solicitudes OPTIONS (preflight)
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    // Responde a la solicitud preflight con un 200 OK y finaliza
+    http_response_code(200);
+    exit;
+}
+
 # RUTEADOR PHP
 # CREDITOS: https://github.com/phprouter/main
 
