7563d36fc268a6b277c23ae1293ebfc813808dc1
jorge-603 <jorgean9999@gmail.com>
2024-11-19 01:21:17 -0600

    Reversión de cambios

--- stats.php
+++ stats.php
@@ -1,13 +1,5 @@
 <?php
 
-# Encabezados CORS
-
-header("Access-Control-Allow-Origin: https://yourfrontenddomain.com"); 
-
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE"); 
-
-header("Access-Control-Allow-Headers: Content-Type, Authorization"); 
-
 # API que permite ver la cantidad de archivos subidos en total
 
 try {
