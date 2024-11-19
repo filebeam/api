040e46867936f6c0bbae9c40ab0fdb2df3a311c2
jorge-603 <jorgean9999@gmail.com>
2024-11-19 01:29:23 -0600

    Encabezados CORS

--- routes.php
+++ routes.php
@@ -2,6 +2,12 @@
 
 require_once __DIR__.'/router.php';
 
+# Encabezados CORS
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
 # RUTEADOR PHP
 # CREDITOS: https://github.com/phprouter/main
 
