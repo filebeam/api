862cb0d33df9f3dfacc6e15d180c07a92cf92e15
jorge-603 <jorgean9999@gmail.com>
2025-04-24 00:23:33 -0600

    Correccion de errores

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Lib\RandomString;
+use App\Lib\Time;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class SubmitController extends Controller
@@ -14,7 +16,7 @@ class SubmitController extends Controller
         $newFileName = new RandomString();
         $newFileName = $newFileName->randomize(6) . '.' . $fileExtension;
 
-        while (file_exists(public_path() . '/file/' . $newFileName)) {
+        while (file_exists(public_path('public') . '/' . $newFileName)) {
             $newFileName = new RandomString();
             $newFileName = $newFileName->randomize(6) . '.' . $fileExtension;
         }
@@ -23,13 +25,34 @@ class SubmitController extends Controller
             $file = $request->file('file');
             $purifiedContent = file_get_contents($file->getPathname());
             $purifiedContent = htmlspecialchars($purifiedContent, ENT_QUOTES | ENT_HTML5);
-            Storage::put('public/' . $newFileName, $purifiedContent);
-            $url = 'https://files.filebeam.xyz/' . $newFileName;
+            Storage::put('.' . $newFileName, $purifiedContent);
+            $url = 'https://' . env('APP_URI_BASE') . "/" . $newFileName;
             return response($url, 200);
         }
 
-        $request->file('file')->storeAs('public', $newFileName);
-        $url = 'https://files.filebeam.xyz/' . $newFileName;
+        $request->file('file')->storeAs('.', $newFileName);
+        $fileHash = hash_file('sha256', $request->file('file'));
+
+        $timestamp = new Time();
+        $timestamp = $timestamp->getUnixTime();
+        $dateTime = new Time();
+        $dateTime = $dateTime->getDateTime();
+        $userAgent = $request->userAgent();
+
+        DB::table('file_logs')->insert([
+            'timestamp' => $timestamp,
+            'date_time' => $dateTime,
+            'user_agent' => $request->userAgent(),
+            'ip_addr' => $request->ip(),
+            'file_name' => $newFileName,
+            'hash' => $fileHash
+        ]);
+
+        if (!$request->isSecure()) {
+        $url = 'http://' . env('APP_URI_BASE') . "/" . $newFileName;
+        return response($url, 200);
+        }
+        $url = 'https://' . env('APP_URI_BASE') . "/" . $newFileName;
         return response($url, 200);
 
     }
