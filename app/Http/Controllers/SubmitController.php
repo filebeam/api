ac8f41989b73f17c57a5cf94917f9bbf441c7c7f
jorge-603 <jorgean9999@gmail.com>
2025-05-30 19:52:40 -0600

    cambios minimos

--- app/Http/Controllers/SubmitController.php
+++ app/Http/Controllers/SubmitController.php
@@ -7,6 +7,7 @@ use App\Lib\RandomString;
 use App\Lib\Time;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
 
 class SubmitController extends Controller
 {
@@ -36,17 +37,138 @@ class SubmitController extends Controller
         $timestamp = new Time();
         $timestamp = $timestamp->getUnixTime();
         $dateTime = new Time();
-        $dateTime = $dateTime->getDateTime();
-        $userAgent = $request->userAgent();
 
-        DB::table('file_logs')->insert([
-            'timestamp' => $timestamp,
-            'date_time' => $dateTime,
-            'user_agent' => $request->userAgent(),
-            'ip_addr' => $request->ip(),
-            'file_name' => $newFileName,
-            'hash' => $fileHash
-        ]);
+
+        # TIEMPOS ADMITIDOS: null (osea tiempo ilimitado), 5m, 30m, 1h, 6h, 12h, 24h
+        switch ($request->input('time')) {
+            case null:
+                # EN CASO DE QUE NO SE ENVÍE NINGUN PARÁMETRO DE TIEMPO (TIEMPO ILIMITADO)
+                # SUBIR LOGS
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+                break;
+            case '5m':
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name'=> $newFileName,
+                    'sent_time'=> $timestamp,
+                    'expire_time'=> $timestamp + 300,
+                ]);
+            break;
+            case '30m':
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name' => $newFileName,
+                    'sent_time' => $timestamp,
+                    'expire_time' => $timestamp + 1800,
+                ]);
+
+            break;
+            case '1h':
+                
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name' => $newFileName,
+                    'sent_time' => $timestamp,
+                    'expire_time' => $timestamp + 3600,
+                ]);
+            
+            break;
+            case '6h':
+                
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name' => $newFileName,
+                    'sent_time' => $timestamp,
+                    'expire_time' => $timestamp + 21600,
+                ]);
+
+            break;
+            case '12h':
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name' => $newFileName,
+                    'sent_time' => $timestamp,
+                    'expire_time' => $timestamp + 43200,
+                ]);
+            break;
+            case '24h':
+                # SUBIDA DE LOG
+                DB::table('file_logs')->insert([
+                'timestamp' => $timestamp,
+                'date_time' => $dateTime->getDateTime(),
+                'user_agent' => $request->userAgent(),
+                'ip_addr' => $request->ip(),
+                'file_name' => $newFileName,
+                'hash' => $fileHash
+                ]);
+
+                # SUBIDA DE PARÁMETRO DE TIEMPO
+                DB::table('tmp_files')->insert([
+                    'file_name' => $newFileName,
+                    'sent_time' => $timestamp,
+                    'expire_time' => $timestamp + 86400,
+                ]);
+            break;
+            default:
+            return response('Parámetro tiempo inválido. Subida cancelada.', 400);
+        }
 
         if (!$request->isSecure()) {
         $url = 'http://' . env('APP_URI_BASE') . "/" . $newFileName;
