aec1b2db3d46e484b2f4cda77d1a4dc21d41eed1
Jorge603 <jorgean9999@gmail.com>
2025-10-04 15:46:17 -0600

    Correccion en ruta /totalFiles

--- routes/api.php
+++ routes/api.php
@@ -6,9 +6,11 @@ use App\Http\Controllers\SubmitController;
 use App\Http\Controllers\AnnouncementsController;
 use App\Http\Middleware\AnnouncementRequests;
 use App\Http\Middleware\ValidateFile;
+use App\Http\Controllers\FilesStatsController;
+use App\Http\Middleware\GetFileStatsRequests;
 
 Route::any('/', [SubmitController::class, 'submit'])->middleware(ValidateFile::class);
 
 Route::any('/anuncios', [AnnouncementsController::class, 'getContent'])->middleware(AnnouncementRequests::class);
 
-Route::any('/totalFiles', []);
+Route::any('/totalFiles', [FilesStatsController::class, 'getFiles'])->middleware(GetFileStatsRequests::class);
