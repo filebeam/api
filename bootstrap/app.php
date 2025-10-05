a24c21f62740153b9a3ca6fa2446fe84493e9ff2
Jorge603 <jorgean9999@gmail.com>
2025-10-04 19:46:54 -0600

    Cambios menores

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Middleware\HandleCors;
 return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->prepend(HandleCors::class);
+        $middleware->append(HandleCors::class);
     })
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
