014d3b9db49c4a4656b8385dc4103da835b908be
Jorge603 <jorgean9999@gmail.com>
2025-10-04 21:59:04 -0600

    cors?

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,11 +4,13 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\HandleCors;
+use App\Http\Middleware\PreflightResponse;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware) {
         //
             $middleware->prepend(HandleCors::class);
+            $middleware->append(PreflightResponse::class);
 
     })
     ->withRouting(
